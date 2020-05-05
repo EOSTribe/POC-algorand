cd7c8f51bda676f6f3649fa93aad1bc0944cf0d7
Yura Kocyuba <yurakocyuba@yandex.ru>
2020-05-05 16:07:17 +0300

    sec

--- main.py
+++ main.py
@@ -1,24 +1,62 @@
 import requests
-import pprint
+from pprint import pprint
 import os
+import json
+import redis
+import time
+import schedule
 
-upstream = os.environ.get('UPSTREAM_API',"http://algo-ua.eostribe.io")
+
+upstream = os.environ.get('UPSTREAM_API',"http://algo.eostribe.io")
+redis_host = os.environ.get('REDIS_HOST',"localhost")
+
+redis_client = redis.Redis(host=redis_host, port=6379, db=0)
+latest_proceeded_round = 0
+nextConsensusVersionRound = 0
 
 def getLastRound():
+    global latest_proceeded_round
+    global nextConsensusVersionRound
     response = requests.get(upstream+'/v1/status')
     if response.status_code == 200:
         r = response.json()
-        return r['lastRound']
-
+        lastRound = r['lastRound']
+        nextConsensusVersionRound=r['nextConsensusVersionRound']
+        if latest_proceeded_round == 0:
+            latest_proceeded_round = lastRound
+            return lastRound
+        else:
+            if lastRound - latest_proceeded_round == 1:
+                latest_proceeded_round = lastRound
+                return lastRound
+            else:
+                print()
+                print("lastRound - latest_proceeded_round: "+str(lastRound - latest_proceeded_round))
+                latest_proceeded_round = lastRound
+                time.sleep(1)
+                return latest_proceeded_round
 
 def getTransactions(lastRound):
-    lastRound = 6476693
     response = requests.get(upstream+'/v1/block/'+str(lastRound))
     if response.status_code == 200:
         r = response.json()
-        transactions = []
+        transactions = r['txns']
+        print(lastRound)
+        pprint(transactions)
+        return transactions
 
 
-getTransactions(getLastRound())
+def job():
+    txs = getTransactions(getLastRound())
+    if txs is not None:
+        if len(txs) >0:
+            redis_client.publish('algo_txs', json.dumps(txs))
+
+schedule.every(4).seconds.do(job)
+
+
+while 1:
+    schedule.run_pending()
+
 
 
